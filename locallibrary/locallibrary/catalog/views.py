e646a3f733a545b8b5449c0ce762077941d80d8b
ddy1996 <galattalant1996@mail.ru>
2022-10-13 09:59:16 +0700

    hello

--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Book, Author, BookInstance, Genre
+from .models import Book, Author, BookInstance
 from django.views import generic
 
 
@@ -9,17 +9,20 @@ def index(request):
 
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     return render(
         request,
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_visits': num_visits},
     )
 
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 3
 
 
 class BookDetailView(generic.DetailView):
